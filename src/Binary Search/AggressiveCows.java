c46f7f31503f8eff6bf0ba8b47ef7fbf83082a77
Kshitij Girkar <43858028+girkarkshitij@users.noreply.github.com>
2022-06-22 17:12:30 +0530

    Create AggressiveCows.java

---
+++ src/Binary Search/AggressiveCows.java
@@ -0,0 +1,202 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class Main {
+    public static void main(String[] xps){
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        StringBuilder sb = new StringBuilder();
+
+        int t = in.Int();
+        while (t-->0){
+            int n = in.Int();
+            int c = in.Int();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = in.Int();
+            }
+            sb.append(solve(n, c, a)).append("\n");
+        }
+        System.out.println(sb);
+        out.close();
+    }
+
+    private static int solve(int n, int c, int[] a) {
+        Arrays.sort(a);
+
+        int lo = 1, hi = a[n-1] - a[0];
+        int ans = 0;
+
+        while(lo <= hi){
+            int mid = lo+(hi-lo)/2;
+            if(ifPossible(a, n, c, mid)){
+                ans = mid;
+                lo = mid+1;
+            }else {
+                hi = mid-1;
+            }
+        }
+
+        return ans;
+    }
+
+    private static boolean ifPossible(int[] a, int n, int cows, int minDistance) {
+        int lastPlacedCow = a[0];
+        int countOfCows = 1;
+
+        for (int i = 1; i < n; i++) {
+            if(a[i]- lastPlacedCow >= minDistance ){
+                lastPlacedCow = a[i];
+                countOfCows++;
+            }
+            if(countOfCows == cows) return true;
+        }
+
+        return false;
+    }
+
+    private static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int Int() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String String() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public double Double() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.') {
+                if (c == 'e' || c == 'E') {
+                    return res * Math.pow(10, Int());
+                }
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.') {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c)) {
+                    if (c == 'e' || c == 'E') {
+                        return res * Math.pow(10, Int());
+                    }
+                    if (c < '0' || c > '9') {
+                        throw new InputMismatchException();
+                    }
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        public long Long() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return String();
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+}
